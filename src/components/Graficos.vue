67d5b14a2b0a942363ef391f61ca55ccfc5e990e
lucas <lucasdesouzasouza57@gmail.com>
2024-10-31 11:08:48 -0300

    Feat: Separando Footer E Header

---
+++ src/components/Graficos.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Foot from '@/components/FoHead/Footer.vue'
+import Head from '@/components/FoHead/Header.vue'
+</script>
+
+<template>
+    <Head />
+
+    <Foot />
+
+</template>
+
+<style setup></style>
