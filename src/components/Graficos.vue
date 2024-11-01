7f5cdd9dc44881ae103dd7cfb0b00bcdfd85b265
lucas <lucasdesouzasouza57@gmail.com>
2024-11-01 11:35:51 -0300

    FEAT: grafico centralizado

--- src/components/Graficos.vue
+++ src/components/Graficos.vue
@@ -14,8 +14,8 @@ export default {
         datasets: [
           {
             label: 'Dados do Gráfico',
-            backgroundColor: ['#f87979', '#7979f8', '#79f879', '#f8f879', '#f879f8'], // Cores das barras
-            data: [40, 20, 12, 30, 25] // Seus dados
+            backgroundColor: ['#88b257', '#4a701c', '#486623', '#284703', '#434d36'], // Cores das barras
+            data: [30, 45, 100, 70, 65] // Seus dados
           }
         ]
       },
@@ -38,11 +38,28 @@ export default {
 </script>
 
 <template>
-  <div style="width: 500px; height: 300px;">
-    <Bar
-      id="my-chart-id"
-      :options="chartOptions"
-      :data="chartData"
-    />
+  <div class="grafico-container">
+    <div class="grafico">
+      <Bar
+        id="my-chart-id"
+        :options="chartOptions"
+        :data="chartData"
+      />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.grafico-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh; /* Isso fará com que o container ocupe toda a altura da viewport */
+}
+
+.grafico {
+  width: 500px; 
+  height: 300px;
+}
+</style>
